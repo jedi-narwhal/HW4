cf49eddf135c097ae91e0825e202abdfc6959fd7
jedi-narwhal <soniasnlep@yahoo.com>
2026-01-29 21:19:40 -0800

    methods for script done

--- mg4-jedi_narwhal/Assets/scripts/gamecontroller.cs
+++ mg4-jedi_narwhal/Assets/scripts/gamecontroller.cs
@@ -7,12 +7,43 @@ public class gamecontroller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //points=0
+        //set frequencey for random.range height
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //check time elaspe-time.time-check google if statement
+        //timeelapse>= frquency-call spawn pipe
     }
+    void SpawnPipes()
+    {
+        //instinate
+    }
+     
+    public static gamecontroller Instance { get; private set; }
+
+
+    private void Awake() {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+
+        }
+        Instance = this;
+    }
+
+    public delegate void Delegate();
+    public event Delegate Score;
+
+    private int _points;
+    void AddPoints()
+    {
+        _points++;
+        Score?.Invoke();
+    }
+
+
 }
--- mg4-jedi_narwhal/Assets/scripts/player.cs
+++ mg4-jedi_narwhal/Assets/scripts/player.cs
@@ -7,12 +7,27 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // may not need this
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //space= jump and allow jump in air
     }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // is grounded when on ground
+    }
+     private void OnTriggerEnter2D(Collider2D col)
+    {
+        //update points
+    }
+
+
+
+
+
+
+
 }
